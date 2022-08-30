de740439dd59209374645cb8465f86b90339a24a
amarachiugwu <marycynthiamara2@gmail.com>
2022-08-30 14:02:39 +0100

    day one tast

---
+++ app2.js
@@ -0,0 +1,7 @@
+const { ethers:ethers } = ethers;
+// const { ethers } = require("ethers");
+// const abi = require('./abi.json');
+// require('dotenv').config();
+
+console.log(ethers);
+console.log("Working");
